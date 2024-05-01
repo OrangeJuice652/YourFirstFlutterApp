142262479746ed6257308f608870ebba96eeb44b
Azamino28ki <miyamizu328@outlook.jp>
2024-05-01 11:00:03 +0900

    smoke.py修正

--- air_test_package/tests/smoke.py
+++ air_test_package/tests/smoke.py
@@ -7,11 +7,13 @@ PKG = os.environ['APP_PACKAGE']
 LOG_DIR = os.environ['LOG_DIR']
 ADB_PATH = os.environ['ADB_PATH']
 SERIAL_NO = os.environ['SERIAL_NO']
-print(f'Android:///{SERIAL_NO}?adb_path={ADB_PATH}')
 auto_setup(devices=[f'Android:///{SERIAL_NO}?adb_path={ADB_PATH}'])
 
+
+print(device().list_app())
+print(os.environ['DEVICEFARM_APP_PATH'])
 if PKG not in device().list_app():
-    install(CWD + "/" + os.environ['APP_PATH'])
+    install(os.environ['DEVICEFARM_APP_PATH'])
 start_app(PKG)
 
 assert_exists(Template(r"tests/statics/top_page.png", "スタート画面があること"))
