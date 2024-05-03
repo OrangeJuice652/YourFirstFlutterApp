bfde01dbb7a498f84354b27bd604a7d3b6bd85a7
Azamino28ki <miyamizu328@outlook.jp>
2024-05-03 11:06:08 +0900

    各UIの確認テストに修正

--- air_test_package/tests/smoke.py
+++ air_test_package/tests/smoke.py
@@ -14,6 +14,35 @@ if PKG not in device().list_app():
     install(os.environ['DEVICEFARM_APP_PATH'])
 start_app(PKG)
 
-# assert_exists(Template(filename=r"tests/statics/top_page.png"))
+try:
+    # 画面左上のアイコンの確認
+    assert_exists(
+        Template(
+            filename=r"tests/statics/home_icon.png",
+            target_pos=1,
+        )
+    )
 
-uninstall(PKG)
+    assert_exists(
+        Template(
+            filename=r"tests/statics/heart_icon.png",
+            target_pos=1,
+        )
+    )
+
+    # 画面中央のボタンの確認
+    assert_exists(
+        Template(
+            filename=r"tests/statics/like_button.png",
+            target_pos=5,
+        )
+    )
+
+    assert_exists(
+        Template(
+            filename=r"tests/statics/next_button.png",
+            target_pos=5,
+        )
+    )
+finally:
+    uninstall(PKG)
